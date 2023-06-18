0d7b19bb403d0411043130488cdf1345553dc1bf
Daniel <daniel_tzonkov@abv.bg>
2023-06-18 16:35:51 +0300

    Update from PC

---
+++ src/TrafficLights/Main.java
@@ -0,0 +1,11 @@
+package TrafficLights;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
