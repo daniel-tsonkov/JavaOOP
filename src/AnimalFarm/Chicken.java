7670b2ed608fefbf49ffd31467d4c5b3f283cd1c
dan <daniel_tzonkov@abv.bg>
2023-06-20 11:33:40 +0300

    Update from TurboX

--- src/AnimalFarm/Chicken.java
+++ src/AnimalFarm/Chicken.java
@@ -5,28 +5,42 @@ public class Chicken {
     private int age;
 
     public Chicken(String name, int age) {
-       setName();
-       setAge();
+        setName(name);
+        setAge(age);
     }
 
     private void setName(String name) {
-        this.name = name;
+        if (name.length() >= 1) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
     }
 
     private void setAge(int age) {
-        this.age = age;
+        if (age >= 0 && age <= 15) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
     }
 
     public double priductPerDay() {
-
+        return this.calculateProductPerDay();
     }
 
     @Override
     public String toString() {
-
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", this.name, this.age, this.priductPerDay());
     }
 
     private double calculateProductPerDay() {
-
+        if (this.age > 0 && this.age < 6) {
+            return 2;
+        } else if (this.age >= 6 && this.age < 11) {
+            return 1;
+        } else {
+            return 0.75;
+        }
     }
 }
