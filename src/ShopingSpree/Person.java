0298d990a795af2e7f9f8a7569b2e5986e4e3f2f
dan <daniel_tzonkov@abv.bg>
2023-06-20 13:12:52 +0300

    Update from TurboX

---
+++ src/ShopingSpree/Person.java
@@ -0,0 +1,5 @@
+package ShopingSpree;
+
+public class Person {
+
+}
---
+++ src/ShopingSpree/Product.java
@@ -0,0 +1,27 @@
+package ShopingSpree;
+
+public class Product {
+    private String name;
+    private double cost;
+
+    public Product(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getName() {
+
+    }
+
+    public double getCost() {
+
+    }
+}
