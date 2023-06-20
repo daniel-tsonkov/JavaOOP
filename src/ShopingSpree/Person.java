89c62d55117bf62b512dee7217288570fb14172a
dan <daniel_tzonkov@abv.bg>
2023-06-20 13:29:59 +0300

    Update from TurboX

--- src/ShopingSpree/Person.java
+++ src/ShopingSpree/Person.java
@@ -1,5 +1,30 @@
 package ShopingSpree;
 
-public class Person {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Person {
+    private String namme;
+    private double money;
+    private List<Product> products = new ArrayList<>();
+
+    public Person(String namme, double money) {
+
+    }
+
+    private void setNamme(String namme) {
+        this.namme = namme;
+    }
+
+    private void setMoney(double money) {
+        this.money = money;
+    }
+
+    public void buyProduct(Product) {
+
+    }
+
+    public String getName() {
+        return namme;
+    }
 }
--- src/ShopingSpree/Product.java
+++ src/ShopingSpree/Product.java
@@ -5,23 +5,31 @@ public class Product {
     private double cost;
 
     public Product(String name, double cost) {
-        this.name = name;
-        this.cost = cost;
+        setName(name);
+        setCost(cost);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    private void setName(String name) {
+        if(!name.isEmpty()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
     }
 
-    public void setCost(double cost) {
-        this.cost = cost;
+    private void setCost(double cost) {
+        if (cost < 0) {
+            this.cost = cost;
+        } else {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
     }
 
     public String getName() {
-
+        return name;
     }
 
     public double getCost() {
-
+        return cost;
     }
 }
