6e1fc84982f0aea7f39db6f9acba21217893afec
dan <daniel_tzonkov@abv.bg>
2023-06-20 10:53:56 +0300

    Update from TurboX

---
+++ src/AnimalFarm/Chicken.java
@@ -0,0 +1,32 @@
+package AnimalFarm;
+
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+       setName();
+       setAge();
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
+    public double priductPerDay() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    private double calculateProductPerDay() {
+
+    }
+}
