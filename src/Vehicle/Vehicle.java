360a0850f56c251a2fbea3935e3637d69e225556
dan <daniel_tzonkov@abv.bg>
2023-06-29 16:28:36 +0300

    UP

---
+++ src/Vehicle/Vehicle.java
@@ -0,0 +1,19 @@
+package Vehicle;
+
+public abstract class Vehicle {
+    private double fuelQuantity;
+    private double fuelConsumption;
+
+    public Vehicle(double fuelQuantity, double fuelConsumption) {
+        this.fuelQuantity = fuelQuantity;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public String drive(double distance) {
+
+    }
+
+    public void refuel(double liters); {
+
+    }
+}
