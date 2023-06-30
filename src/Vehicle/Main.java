f7869f8abf022ac02ace11c6745cfdf0edad648a
dan <daniel_tzonkov@abv.bg>
2023-06-30 16:35:17 +0300

    UP

--- src/Vehicle/Main.java
+++ src/Vehicle/Main.java
@@ -1,5 +1,7 @@
 package Vehicle;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -10,33 +12,54 @@ public class Main {
         double carFuelQuantity = Double.parseDouble(token[1]);
         double carFuelConsumption = Double.parseDouble(token[2]);
 
-        Car car = new Car(carFuelQuantity, carFuelConsumption);
+        Vehicle car = new Car(carFuelQuantity, carFuelConsumption);
 
         token = scanner.next().split("\\s+");
-        double truckFuelQuantity = Double.parseDouble(token[1]);
-        double truckFuelConsumption = Double.parseDouble(token[2]);
+        Vehicle.Vehicle truck = createVehicle(token);
 
-        Truck truck = new Truck(truckFuelQuantity, truckFuelConsumption);
+        Map<String, Vehicle> vehicles = new HashMap<>();
+        vehicles.put("Car", car);
+        vehicles.put("Truck", truck);
 
         int n = Integer.parseInt(scanner.nextLine());
 
         for (int i = 0; i < n; i++) {
-            token[] =scanner.nextLine().split("\\s+");
+            token = scanner.nextLine().split("\\s+");
             String commandName = token[0];
             String vehicleType = token[1];
 
             switch (commandName) {
                 case "Drive":
                     double distance = Double.parseDouble(token[2]);
-                    if (vehicleType.equals("Car")) {
-                        System.out.println(car.drive(distance));
-                    } else {
-                        System.out.println(truck.drive(distance));
-                    }
+                    String driveMessage = vehicles.get(vehicleType).drive(distance);
+                    System.out.println(driveMessage);
                     break;
                 case "Refuel":
+                    double fuelAmount = Double.parseDouble(token[2]);
+                    if (vehicleType.equals("Car")) {
+                        car.refuel(fuelAmount);
+                    } else {
+                        truck.refuel(fuelAmount);
+                    }
                     break;
             }
         }
+    }
+
+    private static Vehicle createVehicle(String[] token) {
+        String vehicleType = token[0];
+        double fuelQuantity = Double.parseDouble(token[1]);
+        double fuelConsumption = Double.parseDouble(token[2]);
+        Vehicle vehicle = null;
+
+        switch (vehicleType) {
+            case "Car":
+                vehicle = new Car(fuelQuantity, fuelConsumption);
+            break;
+            case "Truck":
+                vehicle = new Truck(fuelQuantity, fuelConsumption);
+            break;
+        }
+        return vehicle;
     }
 }
