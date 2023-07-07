156b5e128c4e38e49972c2b04a0537b0f4a35378
Daniel <daniel_tzonkov@abv.bg>
2023-07-07 21:34:47 +0300

    Update from PC

---
+++ src/Hero/Hero.java
@@ -0,0 +1,19 @@
+package Hero;
+
+public class Hero {
+    private String username;
+    private int level;
+
+    public Hero(String username, int level) {
+        this.username = username;
+        this.level = level;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+}
