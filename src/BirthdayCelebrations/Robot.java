fcc050d5a43254c78a47d27966799592c04f9d89
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-07-09 16:10:39 +0300

    update my files

--- src/BirthdayCelebrations/Robot.java
+++ src/BirthdayCelebrations/Robot.java
@@ -4,9 +4,9 @@ public class Robot implements Identifiable {
     private final String id;
     private final String model;
 
-    public Robot(String model, String id) {
-        this.model = model;
+    public Robot(String id, String model) {
         this.id = id;
+        this.model = model;
     }
 
     public String getId() {
