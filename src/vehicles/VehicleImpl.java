7486bc6ba768ea1f04007f7d4f93e6f86e183582
dan <daniel_tzonkov@abv.bg>
2023-07-17 13:46:47 +0300

    UP

---
+++ src/vehicles/VehicleImpl.java
@@ -0,0 +1,14 @@
+package vehicles;
+
+public abstract class VehicleImpl implements Vehicle{
+
+    @Override
+    public String drive(double distance) {
+        return null;
+    }
+
+    @Override
+    public void refuel(double liters) {
+
+    }
+}
