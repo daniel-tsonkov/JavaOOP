05baaf962e9bba637ee66109e8518179ac1059ec
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-07-18 21:51:41 +0300

    UP

---
+++ src/ExamPrep/DatingApp.java
@@ -0,0 +1,13 @@
+package ExamPrep;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class DatingApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
+
+
+    }
+}
