36c213f2d54a5c14759b0fe353748e0d65683ee3
dan <daniel_tzonkov@abv.bg>
2023-07-18 10:15:41 +0300

    UP

--- src/cafe/Cafe.java
+++ src/cafe/Cafe.java
@@ -22,22 +22,22 @@ public class Cafe {
     }
 
     public boolean removeEmployee(String name) {
-        if (this.employees.contains(name)) {
-            this.employees.remove(name);
+       /* if (this.employees.contains(name)) {
+            this.employees.remove(employee);
             return true;
         } else {
             return false;
-        }
-        /*for (Employee employee : this.employees) {
+        }*/
+        for (Employee employee : this.employees) {
             if(employee.getName().equals(name)) {
-                this.employees.remove(name);
+                this.employees.remove(employee);
                 return true;
             }
         }
-        return false;*/
+        return false;
     }
 
-    public Employee getOldestEmployee(){
+    public Employee getOldestEmployee() {
         return this.employees.stream().max((e1, e2) -> Integer.compare(e1.getAge(), e2.getAge())).get();
 
         /*Employee oldestEmploee = new Employee("", 0, "");
@@ -50,6 +50,26 @@ public class Cafe {
     }
 
     public Employee getEmployee(String name) {
+        for (Employee employee : this.employees) {
+            if (employee.getName().equals(name)) {
+                return employee;
+            }
+        }
+        return null;
+    }
 
+    public int getCount() {
+        return this.employees.size();
+    }
+
+    public String report() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Employees working at Cafe " + this.name + ": ").append(System.lineSeparator());
+        /*for (Employee employee : this.employees) {
+            sb.append(employee.toString()).append(System.lineSeparator());
+        }*/
+        this.employees.forEach(e -> sb.append(e.toString()).append(System.lineSeparator()));
+
+        return sb.toString();
     }
 }
