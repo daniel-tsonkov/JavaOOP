c8d52f4a6a9a6f18d410259e1a67668ad572e6b6
dan <daniel_tzonkov@abv.bg>
2023-07-18 12:27:45 +0300

    UP

---
+++ src/ExamPrep/SantasPresentFactory.java
@@ -0,0 +1,14 @@
+package ExamPrep;
+
+import java.util.Scanner;
+
+public class SantasPresentFactory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String matterialsInput = scanner.nextLine();
+        String magicsInput = scanner.nextLine();
+
+
+    }
+}
