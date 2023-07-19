6b66244b7a964bbfed405d77b3b92b963aed8935
dan <daniel_tzonkov@abv.bg>
2023-07-19 15:24:54 +0300

    UP

--- src/parking/Parking.java
+++ src/parking/Parking.java
@@ -1,6 +1,7 @@
 package parking;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Parking {
@@ -16,7 +17,7 @@ public class Parking {
     }
 
     public void add(Car car) {
-        if (this.capacity < this.data.size()) {
+        if (this.capacity > this.data.size()) {
             this.data.add(car);
             this.capacity--;
         }
@@ -24,5 +25,28 @@ public class Parking {
 
     public boolean remove(String manufacturer, String model) {
         return this.data.removeIf(car -> car.getManufacturer().equals(manufacturer) && car.getModel().equals(model));
+    }
+
+    public Car getLatestCar() {
+        return this.data.stream().max(Comparator.comparingInt(Car::getYear)).orElse(null);
+    }
+
+    public Car getCar(String manufacturer, String model) {
+        return this.data.stream().filter(car -> car.getManufacturer().equals(manufacturer) && car.getModel().equals(model)).findFirst().orElse(null);
+    }
+
+    public int getCount() {
+        return this.data.size();
+    }
+
+    public String getStatistics() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("The cars are parked in %s: ", this.type));
+        this.data.forEach(car -> {
+            sb.append(System.lineSeparator());
+            sb.append(car.toString());
+        });
+
+        return sb.toString();
     }
 }
