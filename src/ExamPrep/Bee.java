4019e8365c443f4ab1e26228f1947ddb7d83d93f
dan <daniel_tzonkov@abv.bg>
2023-07-19 11:35:30 +0300

    UP

--- src/ExamPrep/Bee.java
+++ src/ExamPrep/Bee.java
@@ -6,6 +6,31 @@ public class Bee {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+        String[][] field = new String[n][n];
+        int beeCoordinatesX = -1;
+        int beeCoordinatesY = -1;
 
+        for (int i = 0; i < n; i++) {
+            field[i] = scanner.nextLine().split("");
+            for (int j = 0; j < n; j++) {
+                if (field[i][j].equals("B")) {
+                    beeCoordinatesX = i;
+                    beeCoordinatesY = j;
+                }
+
+            }
+        }
+
+        printMatrix(field);
+    }
+
+    private static void printMatrix(String[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
