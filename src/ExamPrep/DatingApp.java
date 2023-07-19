83b48226d4905fd3cd741524dd9ac5af636211fa
dan <daniel_tzonkov@abv.bg>
2023-07-19 10:29:12 +0300

    UP

--- src/ExamPrep/DatingApp.java
+++ src/ExamPrep/DatingApp.java
@@ -1,5 +1,8 @@
 package ExamPrep;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.Scanner;
 
 import static java.lang.System.in;
@@ -7,7 +10,67 @@ import static java.lang.System.in;
 public class DatingApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(in);
+        int matches = 0;
 
+        Deque<Integer> maleStack = new ArrayDeque<>();
 
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .forEach(maleStack::push);
+
+        Deque<Integer> femaleQueue = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .forEach(femaleQueue::offer);
+
+        while (!maleStack.isEmpty() && !femaleQueue.isEmpty()) {
+            int male = maleStack.peek();
+            int female = femaleQueue.peek();
+
+            if (female <= 0) {
+                femaleQueue.poll();
+                continue;
+            } else if(male <= 0) {
+                maleStack.pop();
+                continue;
+            }
+
+            if(female % 25 == 0) {
+                femaleQueue.poll();
+                femaleQueue.poll();
+                continue;
+            }else if (male % 25 == 0) {
+                maleStack.pop();
+                maleStack.pop();
+                continue;
+            }
+
+            if (male == female) {
+                matches++;
+                maleStack.pop();
+                femaleQueue.poll();
+            } else {
+                femaleQueue.poll();
+                int changeValueOfMale = maleStack.pop() - 2;
+                maleStack.push(changeValueOfMale);
+            }
+        }
+
+        System.out.printf("Matches: %d\n", matches);
+
+        System.out.print("Males left: ");
+        if(!maleStack.isEmpty()) {
+            System.out.println(maleStack.toString().replace("[", "").replace("]", ""));
+        } else {
+            System.out.println("none");
+        }
+
+        System.out.print("Females left: ");
+        if(!femaleQueue.isEmpty()) {
+            System.out.println(femaleQueue.toString().replace("[", "").replace("]", ""));
+        } else {
+            System.out.println("none");
+        }
     }
 }
--- src/ExamPrep/SantasPresentFactory.java
+++ src/ExamPrep/SantasPresentFactory.java
@@ -1,6 +1,7 @@
 package ExamPrep;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SantasPresentFactory {
     public static void main(String[] args) {
@@ -9,6 +10,80 @@ public class SantasPresentFactory {
         String matterialsInput = scanner.nextLine();
         String magicsInput = scanner.nextLine();
 
+        Stack<Integer> matterials = new Stack<>();
+        Arrays.stream(matterialsInput.split("\\s+")).map(Integer::parseInt).forEach(matterials::push);
 
+        Queue<Integer> magics = new ArrayDeque<>();
+        magics = Arrays.stream(magicsInput.split("\\s+")).map(Integer::parseInt).collect(Collectors.toCollection(ArrayDeque::new));
+
+        Map<String, Integer> presentsMap = new TreeMap<>();
+        presentsMap.put("Doll", 0);
+        presentsMap.put("Wooden train", 0);
+        presentsMap.put("Teddy bear", 0);
+        presentsMap.put("Bicycle", 0);
+
+
+        while (!matterials.isEmpty() && !magics.isEmpty()) {
+            int material = matterials.peek();
+            int magic = magics.peek();
+
+            int totalMagic = material * magic;
+
+            if(totalMagic == 150) {
+                matterials.pop();
+                magics.poll();
+                presentsMap.put("Doll", presentsMap.get("Doll") + 1);
+            }else if(totalMagic == 250) {
+                matterials.pop();
+                magics.poll();
+                presentsMap.put("Wooden train", presentsMap.get("Wooden train") + 1);
+            }else if(totalMagic == 300) {
+                matterials.pop();
+                magics.poll();
+                presentsMap.put("Teddy bear", presentsMap.get("Teddy bear") + 1);
+            }else if(totalMagic == 400) {
+                matterials.pop();
+                magics.poll();
+                presentsMap.put("Bicycle", presentsMap.get("Bicycle") + 1);
+            } else if (totalMagic < 0) {
+                int sum = material + magic;
+                matterials.pop();
+                magics.poll();
+                matterials.push(sum);
+            } else if (totalMagic > 0) {
+                magics.poll();
+                matterials.push(matterials.pop() + 15);
+            } else {
+                if(material == 0) {
+                    matterials.pop();
+                }
+                if(magic == 0) {
+                    magics.poll();
+                }
+            }
+        }
+
+        if ((presentsMap.get("Doll") > 0 && presentsMap.get("Wooden train") > 0) || (presentsMap.get("Teddy bear") > 0 && presentsMap.get("Bicycle") > 0)) {
+            System.out.println("The presents are crafted! Merry Christmas!");
+        } else {
+            System.out.println("No presents this Christmas!");
+        }
+
+        if (!matterials.isEmpty()) {
+            System.out.print("Materials left: ");
+            Collections.reverse(matterials);
+            System.out.println(matterials.toString().replace("[", "").replace("]", ""));
+        }
+
+        if (!magics.isEmpty()) {
+            System.out.print("Magic left: ");
+            System.out.println(magics.toString().replace("[", "").replace("]", ""));
+        }
+
+        for (Map.Entry<String, Integer> entry : presentsMap.entrySet()) {
+            if (entry.getValue() > 0) {
+                System.out.printf("%s: %d\n", entry.getKey(), entry.getValue());
+            }
+        }
     }
 }
