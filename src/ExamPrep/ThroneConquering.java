86d2867a2f0ea5f0266218635819c45290f5f60c
dan <daniel_tzonkov@abv.bg>
2023-07-21 15:11:37 +0300

    UP

--- src/ExamPrep/ThroneConquering.java
+++ src/ExamPrep/ThroneConquering.java
@@ -8,31 +8,38 @@ public class ThroneConquering {
 
         int parisEnergy = Integer.parseInt(scanner.nextLine());
         boolean parisLeftEnergy = true;
-        int n = Integer.parseInt(scanner.nextLine());
+        int rows = Integer.parseInt(scanner.nextLine());
+        String[][] field = new String[rows][];
+        int col = 0;
 
-        String[][] field = new String[n][n];
+
+        for (int i = 0; i < rows; i++) {
+            field[i] = scanner.nextLine().split("");
+        }
+
 
         int parisRow = -1;
         int parisCol = -1;
 
-        for (int i = 0; i < n; i++) {
-            field[i] = scanner.nextLine().split("");
-            for (int j = 0; j < n; j++) {
-                if (field[i][j].equals("P")) {
-                    parisRow = i;
-                    parisCol = j;
+        for (int row = 0; row < rows; row++) {
+            for (int cols = 0; cols < field[row].length; cols++) {
+                if (field[row][cols] == "P") {
+                    parisRow = row;
+                    parisCol = col;
                 }
             }
         }
+
 
         while (parisLeftEnergy) {
             String[] input = scanner.nextLine().split("\\s+");
             String command = input[0];
             int spartanRow = Integer.parseInt(input[1]);
             int spartanCol = Integer.parseInt(input[2]);
-            field[spartanRow][spartanCol] = "S";
             int startRow = parisRow;
             int startCol = parisCol;
+            field[startRow][startCol] = "-";
+            field[spartanRow][spartanCol] = "S";
 
             switch (command) {
                 case "up":
@@ -43,7 +50,7 @@ public class ThroneConquering {
                     break;
                 case "down":
                     parisRow++;
-                    if (parisRow >= n) {
+                    if (parisRow >= field.length) {
                         parisRow--;
                     }
                     break;
@@ -55,7 +62,7 @@ public class ThroneConquering {
                     break;
                 case "right":
                     parisCol++;
-                    if (parisCol >= n) {
+                    if (parisCol >= field.length) {
                         parisCol--;
                     }
                     break;
@@ -63,25 +70,23 @@ public class ThroneConquering {
 
             parisEnergy--;
 
-            field[startRow][startCol] = "-";
-
-            if (field[parisRow][parisCol].equals("S")) {
-                parisEnergy -= 2;
-            }
-
-            if (field[parisRow][parisCol].equals("H")) {
-                field[parisRow][parisCol] = "-";
-                parisLeftEnergy = false;
-                System.out.printf("Paris has successfully abducted Helen! ");
-                System.out.printf("Energy left: %d\n", parisEnergy);
-            } else {
-                field[parisRow][parisCol] = "P";
-            }
-
             if (parisEnergy <= 0) {
-                field[parisRow][parisCol] = "X";
-                parisLeftEnergy = false;
-                System.out.printf("Paris died at %d;%d.\n", parisRow, parisCol);
+                parisDead(field, parisRow, parisCol);
+                return;
+            }
+            if (field[parisRow][parisCol] == "S") {
+                parisEnergy -= 2;
+                if (parisEnergy <= 0) {
+                    parisDead(field, parisRow, parisCol);
+                    return;
+                } else {
+                    field[parisRow][parisCol] = "-";
+                }
+            } else if (field[parisRow][parisCol] == "H") {
+                field[parisRow][parisCol] = "-";
+                System.out.printf("Paris has successfully abducted Helen! Energy left: %d%n", parisEnergy);
+                printMatrix(field);
+                return;
             }
         }
 
@@ -89,10 +94,16 @@ public class ThroneConquering {
 
     }
 
-    private static void printMatrix(String[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.print(matrix[i][j]);
+    public static void parisDead(String[][] field, int parisRow, int parisCol) {
+        field[parisRow][parisCol] = "X";
+        System.out.printf("Paris died at %d;%d.%n", parisRow, parisCol);
+        printMatrix(field);
+    }
+
+    private static void printMatrix(String[][] field) {
+        for (int row = 0; row < field.length; row++) {
+            for (int col = 0; col < field[row].length; col++) {
+                System.out.print(field[row][col]);
             }
             System.out.println();
         }
