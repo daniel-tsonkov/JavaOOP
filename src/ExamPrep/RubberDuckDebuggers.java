08e210da6a74a6d43d68f416a64d282ee199ac2e
dan <daniel_tzonkov@abv.bg>
2023-07-31 16:03:11 +0300

    UP

---
+++ src/ExamPrep/RubberDuckDebuggers.java
@@ -0,0 +1,11 @@
+package ExamPrep;
+
+import java.util.Scanner;
+
+public class RubberDuckDebuggers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
